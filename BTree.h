be1ed5f602d1e033d1e1493d44c5b2272ad9409b
eichitang <34444201+eichitang@users.noreply.github.com>
2019-07-17 15:53:39 +0800

    Update BTree.h

--- BTree.h
+++ BTree.h
@@ -23,8 +23,13 @@ class BTree{
 		int size() const;
 		//bool opeator>(std::pair<const K, E>& thePair) const;
 		void insert(const std::pair<const K, E>& thePair);
+		E SearchTree(BTreeNode<std::pair<K,E>> *r,K& theKey);
+		int SearchNode(BTreeNode<std::pair<K,E>> *r,K& k);
+		E find( K theKey) ;
 		void output();
 		void outTest();
+		void BTree_print(BTreeNode<std::pair<K,E>> *r, BTreeNode<std::pair<K,E>> *pc,int layer);
+		void print();
 		int BTreeMaximum(BTreeNode<std::pair<K,E>> *x );
 		int BTreeMinimum( BTreeNode<std::pair<K,E>> *x );
 		void BTreeDelete( BTreeNode<std::pair<K,E>> *&x, int k );
@@ -32,10 +37,11 @@ class BTree{
 		BTreeNode<std::pair<K,E>> *root;
 		int treeSize;
 		int rank;
-		BTreeNode<std::pair<K,E>> pointerCollector;
+		BTreeNode<std::pair<K,E>> *pointerCollector;
 
 
 };
+
 
 template <class K, class E>
 std::ostream& operator<<(std::ostream& out,std::pair<K, E>& x){
